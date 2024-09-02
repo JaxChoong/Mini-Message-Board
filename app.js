1a4ae23ab604036c7d7717d64ae55a9f6e81e885
Jax Choong <choongjax@gmail.com>
2024-09-02 22:35:31 +0800

    fix: changed port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("node:path")
 
 const app = express()
-const PORT = 3000
+const PORT = 2999
 
 // setup templating with ejs
 app.set("views",path.join(__dirname,"views"))
